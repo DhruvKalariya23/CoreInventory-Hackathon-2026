f7a23771edcd3b6fcac0ebf6039ec5db591325bd
Dhruv <dkalariya374@gmail.com>
2026-03-14 12:50:55 +0530

    [MOD] fix db issue.

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,29 @@
-const { Pool } = require("pg")
+const { Client } = require("pg")
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "coreinventory",
-  password: "postgres",
-  port: 5432
-})
+async function createDatabase() {
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    password: "postgres",
+    port: 5432,
+    database: "postgres" // connect to default db first
+  })
 
-module.exports = pool
+  await client.connect()
+
+  const res = await client.query(
+    "SELECT 1 FROM pg_database WHERE datname='coreinventory'"
+  )
+
+  if (res.rowCount === 0) {
+    console.log("Creating database coreinventory...")
+    await client.query("CREATE DATABASE coreinventory")
+    console.log("Database created successfully")
+  } else {
+    console.log("Database already exists")
+  }
+
+  await client.end()
+}
+
+module.exports = createDatabase
